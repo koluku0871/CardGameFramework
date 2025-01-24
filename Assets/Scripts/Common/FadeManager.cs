930330e0fab57b541cd3fbc2520a1364fc12e55a
koluku <nttkolukuaquosphone@gmail.com>
2025-01-24 14:41:23 +0900

    [Add]Fade

---
+++ Assets/Scripts/Common/FadeManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FadeManager : MonoBehaviour
+{
+    [SerializeField]
+    Fade fade = null;
+
+    private static FadeManager instance = null;
+    public static FadeManager Instance()
+    {
+        return instance;
+    }
+
+    public void Awake()
+    {
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void OnStart(string sceneName)
+    {
+        StartCoroutine(LoadSceneAsync(sceneName));
+    }
+
+    IEnumerator LoadSceneAsync(string sceneName)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+        asyncLoad.allowSceneActivation = false;
+
+        fade.FadeIn(0.5f, () =>
+        {
+            asyncLoad.allowSceneActivation = true;
+        });
+        
+        while (!asyncLoad.isDone)
+        {
+            Debug.Log(asyncLoad.progress * 100f + "%ì«Ç›çûÇ›äÆóπ");
+            yield return null;
+        }
+
+        fade.FadeOut(0.5f);
+    }
+}

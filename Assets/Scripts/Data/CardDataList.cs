43ef3eb21c26c667d67d8ab1d18d18fa3cbf97a5
koluku <nttkolukuaquosphone@gmail.com>
2025-01-27 20:58:46 +0900

    [Delete]不要クラスを破棄

--- Assets/Scripts/Data/CardDataList.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using System.IO;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "DB/CardDataList")]
-public class CardDataList : ScriptableObject
-{
-    public List<CardData> cardDatas = new List<CardData>();
-}
--- Assets/Scripts/Data/PackData.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Collections.Generic;
-
-public class PackData {
-    public string tag = "";
-    public List<string> fileNameList = new List<string>();
-}

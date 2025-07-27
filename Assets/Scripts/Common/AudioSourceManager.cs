49c0a8989d2438aa84d847836ad636692519f03a
koluku <nttkolukuaquosphone@gmail.com>
2025-07-28 01:08:14 +0900

    [Update]設定ファイルの音量設定を反映するように修正

--- Assets/Scripts/Common/AudioSourceManager.cs
+++ Assets/Scripts/Common/AudioSourceManager.cs
@@ -37,13 +37,30 @@ public class AudioSourceManager : MonoBehaviour
 
     public void PlayOneShot(int index, bool isBgm = false)
     {
+        OptionData optionData = new OptionData();
+        optionData.IsFileExists();
+        optionData.LoadTxt();
+
         if (isBgm)
         {
             m_audioSourceBgm.Stop();
             m_audioSourceBgm.clip = m_soundBgmList[index];
+            switch (index)
+            {
+                case 0:
+                    m_audioSourceSe.volume = optionData.battleBgmVolume;
+                    break;
+                case 1:
+                    m_audioSourceSe.volume = optionData.homeBgmVolume;
+                    break;
+                case 2:
+                    m_audioSourceSe.volume = optionData.deckBgmVolume;
+                    break;
+            }
             m_audioSourceBgm.Play();
             return;
         }
+        m_audioSourceSe.volume = optionData.seVolume;
         m_audioSourceSe.PlayOneShot(m_soundSeList[index]);
     }
 }

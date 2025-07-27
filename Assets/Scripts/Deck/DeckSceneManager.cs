078cc8ec3eeab842c16b4a87482e650b7dab91e2
koluku <nttkolukuaquosphone@gmail.com>
2025-07-28 01:23:39 +0900

    [Update]フィルタ情報の設定を専用ウインドウ上の操作に変更　項目を追加

--- Assets/Scripts/Deck/DeckSceneManager.cs
+++ Assets/Scripts/Deck/DeckSceneManager.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using WebSocketSharp;
 
 public class DeckSceneManager : MonoBehaviour
 {
@@ -19,14 +21,40 @@ public class DeckSceneManager : MonoBehaviour
     [SerializeField]
     private Dropdown m_packTypeDropdown = null;
 
+    [Header("カードフィルター")]
+
     [SerializeField]
     private Dropdown m_elementDropdown = null;
+
+    [SerializeField]
+    private Toggle m_elementToggle = null;
 
     [SerializeField]
     private Dropdown m_categoryDropdown = null;
 
     [SerializeField]
+    private Toggle m_categoryToggle = null;
+
+    [SerializeField]
     private Dropdown m_cardTypeDropdown = null;
+
+    [SerializeField]
+    private Toggle m_typeToggle = null;
+
+    [SerializeField]
+    private Dropdown m_cardSubTypeDropdown = null;
+
+    [SerializeField]
+    private Toggle m_subTypeToggle = null;
+
+    [SerializeField]
+    private Dropdown m_cardCostDropdown = null;
+
+    [SerializeField]
+    private List<Toggle> m_costToggleList = new List<Toggle>();
+
+    [SerializeField]
+    private Dropdown m_cardRarityDropdown = null;
 
     [SerializeField]
     private TMP_InputField m_cardNameInputField = null;
@@ -88,6 +116,10 @@ public class DeckSceneManager : MonoBehaviour
             return null;
         }
         return cardDatas[index];
+    }
+    public List<CardData> GetCardDatas()
+    {
+        return cardDatas;
     }
     private int oldSelectId = -1;
 
@@ -255,13 +287,10 @@ public class DeckSceneManager : MonoBehaviour
         m_packTypeDropdown.value = -1;
 
         m_elementDropdown.ClearOptions();
-        foreach (var item in ConstManager.CardElementType.Values)
+        m_elementDropdown.options.Add(new Dropdown.OptionData()
         {
-            m_elementDropdown.options.Add(new Dropdown.OptionData()
-            {
-                text = item
-            });
-        }
+            text = "All"
+        });
         m_elementDropdown.value = -1;
 
         m_cardTypeDropdown.ClearOptions();
@@ -271,12 +300,33 @@ public class DeckSceneManager : MonoBehaviour
         });
         m_cardTypeDropdown.value = -1;
 
+        m_cardSubTypeDropdown.ClearOptions();
+        m_cardSubTypeDropdown.options.Add(new Dropdown.OptionData()
+        {
+            text = "All"
+        });
+        m_cardSubTypeDropdown.value = -1;
+
         m_categoryDropdown.ClearOptions();
         m_categoryDropdown.options.Add(new Dropdown.OptionData()
         {
             text = "All"
         });
         m_categoryDropdown.value = -1;
+
+        m_cardCostDropdown.ClearOptions();
+        m_cardCostDropdown.options.Add(new Dropdown.OptionData()
+        {
+            text = "All"
+        });
+        m_cardCostDropdown.value = -1;
+
+        m_cardRarityDropdown.ClearOptions();
+        m_cardRarityDropdown.options.Add(new Dropdown.OptionData()
+        {
+            text = "All"
+        });
+        m_cardRarityDropdown.value = -1;
 
         OnValueChangedToPackTypeDropdown();
     }
@@ -285,10 +335,44 @@ public class DeckSceneManager : MonoBehaviour
     {
         var key = m_packTypeDropdown.options[m_packTypeDropdown.value].text;
         AssetBundleManager assetBundleManager = AssetBundleManager.Instance();
+
+        List<string> packNameList = new List<string>();
         if (!assetBundleManager.AssetBundleBaseCardData.baseData.packNameList.ContainsKey(key))
         {
+            foreach (var packName in assetBundleManager.AssetBundleBaseCardData.baseData.packNameList)
+            {
+                packNameList.AddRange(packName.Value);
+            }
         }
-        return (key, assetBundleManager.AssetBundleBaseCardData.baseData.packNameList[key]);
+        else
+        {
+            packNameList = assetBundleManager.AssetBundleBaseCardData.baseData.packNameList[key];
+        }
+        return (key, packNameList);
+    }
+
+    public Dictionary<string, List<string>> GetPackNameListFromDictionary()
+    {
+        Dictionary<string, List<string>> packNameList = new Dictionary<string, List<string>>();
+
+        var key = m_packTypeDropdown.options[m_packTypeDropdown.value].text;
+        AssetBundleManager assetBundleManager = AssetBundleManager.Instance();
+        if (key == "All" || !assetBundleManager.AssetBundleBaseCardData.baseData.packNameList.ContainsKey(key))
+        {
+            foreach (var list in assetBundleManager.AssetBundleBaseCardData.baseData.packNameList)
+            {
+                if (list.Key == "All")
+                {
+                    continue;
+                }
+                packNameList.Add(list.Key, list.Value);
+            }
+        }
+        else
+        {
+            packNameList.Add(key, assetBundleManager.AssetBundleBaseCardData.baseData.packNameList[key]);
+        }
+        return packNameList;
     }
 
     public void OnValueChangedToPackTypeDropdown()
@@ -315,14 +399,21 @@ public class DeckSceneManager : MonoBehaviour
         CardDetailManager cardDetailManager = CardDetailManager.Instance();
 
         int searchCardCount = 0;
-        HashSet<string> cardTypeList = new HashSet<string>();
-        HashSet<string> categoryList = new HashSet<string>();
+        SortedSet<string> elementList = new SortedSet<string>();
+        SortedSet<string> cardTypeList = new SortedSet<string>();
+        SortedSet<string> cardSubTypeList = new SortedSet<string>();
+        SortedSet<string> categoryList = new SortedSet<string>();
+        SortedSet<string> costList = new SortedSet<string>();
+        SortedSet<string> rarityList = new SortedSet<string>();
 
         if (oldSelectId != selectId)
         {
             m_elementDropdown.value = 0;
             m_cardTypeDropdown.value = 0;
+            m_cardSubTypeDropdown.value = 0;
             m_categoryDropdown.value = 0;
+            m_cardCostDropdown.value = 0;
+            m_cardRarityDropdown.value = 0;
             m_cardNameInputField.text = "";
         }
         bool isAll = key == "All";
@@ -346,6 +437,11 @@ public class DeckSceneManager : MonoBehaviour
 
             if (oldSelectId != selectId)
             {
+                if (!string.IsNullOrEmpty(cardData.Element))
+                {
+                    elementList.Add(cardData.Element.Replace("\r", "").Replace("\n", ""));
+                }
+
                 if (!string.IsNullOrEmpty(cardData.CardType))
                 {
                     var cardTypeSplit = cardData.CardType.Split("・");
@@ -356,15 +452,62 @@ public class DeckSceneManager : MonoBehaviour
                             cardTypeList.Add(item);
                         }
                     }
-                    else if (!string.IsNullOrEmpty(cardData.CardType))
+                    else
                     {
-                        cardTypeList.Add(cardData.CardType);
+                        cardTypeSplit = cardData.CardType.Split("／");
+                        if (cardTypeSplit.Length > 1)
+                        {
+                            foreach (var item in cardTypeSplit)
+                            {
+                                cardTypeList.Add(item);
+                            }
+                        }
+                        else
+                        {
+                            cardTypeList.Add(cardData.CardType);
+                        }
+                    }
+                }
+                if (!string.IsNullOrEmpty(cardData.CardSubType))
+                {
+                    var cardTypeSplit = cardData.CardSubType.Split("／");
+                    if (cardTypeSplit.Length > 1)
+                    {
+                        foreach (var item in cardTypeSplit)
+                        {
+                            cardSubTypeList.Add(item);
+                        }
+                    }
+                    else
+                    {
+                        cardSubTypeList.Add(cardData.CardSubType);
                     }
                 }
 
                 if (!string.IsNullOrEmpty(cardData.CardCategory))
                 {
-                    categoryList.Add(cardData.CardCategory);
+                    var split = cardData.CardCategory.Split("／");
+                    if (split.Length > 1)
+                    {
+                        foreach (var item in split)
+                        {
+                            categoryList.Add(item);
+                        }
+                    }
+                    else
+                    {
+                        categoryList.Add(cardData.CardCategory);
+                    }
+                }
+
+                if (!string.IsNullOrEmpty(cardData.Cost))
+                {
+                    costList.Add(cardData.Cost);
+                }
+
+                if (!string.IsNullOrEmpty(cardData.CardRarity))
+                {
+                    rarityList.Add(cardData.CardRarity);
                 }
             }
 
@@ -374,7 +517,39 @@ public class DeckSceneManager : MonoBehaviour
             }
             cardDatas.Add(cardData);
         }
+        if (oldSelectId != selectId && AssetBundleManager.Instance().CardType == "yugioh")
+        {
+            categoryList.Add("融合＊シンクロ＊エクシーズ＊リンク");
+            categoryList.Add("融合＊シンクロ＊エクシーズ");
+            categoryList.Add("融合＊シンクロ＊リンク");
+            categoryList.Add("融合＊エクシーズ＊リンク");
+            categoryList.Add("シンクロ＊エクシーズ＊リンク");
+            categoryList.Add("融合＊シンクロ");
+            categoryList.Add("融合＊エクシーズ");
+            categoryList.Add("融合＊リンク");
+            categoryList.Add("シンクロ＊エクシーズ");
+            categoryList.Add("シンクロ＊リンク");
+            categoryList.Add("エクシーズ＊リンク");
+        }
+
         searchCardCount = cardDatas.Count;
+
+        if (elementList.Count > 0)
+        {
+            m_elementDropdown.ClearOptions();
+            m_elementDropdown.options.Add(new Dropdown.OptionData()
+            {
+                text = "All"
+            });
+            foreach (var item in elementList)
+            {
+                m_elementDropdown.options.Add(new Dropdown.OptionData()
+                {
+                    text = item
+                });
+            }
+            m_elementDropdown.value = -1;
+        }
 
         if (cardTypeList.Count > 0)
         {
@@ -392,6 +567,24 @@ public class DeckSceneManager : MonoBehaviour
             }
             m_cardTypeDropdown.value = -1;
         }
+
+        if (cardSubTypeList.Count > 0)
+        {
+            m_cardSubTypeDropdown.ClearOptions();
+            m_cardSubTypeDropdown.options.Add(new Dropdown.OptionData()
+            {
+                text = "All"
+            });
+            foreach (var item in cardSubTypeList)
+            {
+                m_cardSubTypeDropdown.options.Add(new Dropdown.OptionData()
+                {
+                    text = item
+                });
+            }
+            m_cardSubTypeDropdown.value = -1;
+        }
+
         if (categoryList.Count > 0)
         {
             m_categoryDropdown.ClearOptions();
@@ -408,6 +601,41 @@ public class DeckSceneManager : MonoBehaviour
             }
             m_categoryDropdown.value = -1;
         }
+
+        if (costList.Count > 0)
+        {
+            m_cardCostDropdown.ClearOptions();
+            m_cardCostDropdown.options.Add(new Dropdown.OptionData()
+            {
+                text = "All"
+            });
+            foreach (var item in costList)
+            {
+                m_cardCostDropdown.options.Add(new Dropdown.OptionData()
+                {
+                    text = item
+                });
+            }
+            m_cardCostDropdown.value = -1;
+        }
+
+        if (rarityList.Count > 0)
+        {
+            m_cardRarityDropdown.ClearOptions();
+            m_cardRarityDropdown.options.Add(new Dropdown.OptionData()
+            {
+                text = "All"
+            });
+            foreach (var item in rarityList)
+            {
+                m_cardRarityDropdown.options.Add(new Dropdown.OptionData()
+                {
+                    text = item
+                });
+            }
+            m_cardRarityDropdown.value = -1;
+        }
+
         m_searchCardScrollSetup.max = (cardDatas.Count / 4);
         if (cardDatas.Count % 4 != 0)
         {
@@ -429,7 +657,7 @@ public class DeckSceneManager : MonoBehaviour
 
         if (m_elementDropdown.value > 0)
         {
-            if (!cardData.Element.Contains(ConstManager.CardElementType[m_elementDropdown.value]))
+            if ((!cardData.Element.Replace("\r", "").Replace("\n", "").Contains(m_elementDropdown.options[m_elementDropdown.value].text)) != m_elementToggle.isOn)
             {
                 return false;
             }
@@ -437,7 +665,15 @@ public class DeckSceneManager : MonoBehaviour
 
         if (m_cardTypeDropdown.value > 0)
         {
-            if (!cardData.CardType.Contains(m_cardTypeDropdown.options[m_cardTypeDropdown.value].text))
+            if ((!cardData.CardType.Contains(m_cardTypeDropdown.options[m_cardTypeDropdown.value].text) != m_typeToggle.isOn))
+            {
+                return false;
+            }
+        }
+
+        if (m_cardSubTypeDropdown.value > 0)
+        {
+            if ((!cardData.CardSubType.Contains(m_cardSubTypeDropdown.options[m_cardSubTypeDropdown.value].text) != m_subTypeToggle.isOn))
             {
                 return false;
             }
@@ -445,7 +681,87 @@ public class DeckSceneManager : MonoBehaviour
 
         if (m_categoryDropdown.value > 0)
         {
-            if (!cardData.CardCategory.Contains(m_categoryDropdown.options[m_categoryDropdown.value].text))
+            var category = m_categoryDropdown.options[m_categoryDropdown.value].text.Split("＊");
+            if (category.Count() > 1)
+            {
+                bool isBreak = false;
+                foreach (var c in category)
+                {
+                    if (cardData.CardCategory.Contains(c))
+                    {
+                        isBreak = true;
+                        break;
+                    }
+                }
+                if ((!isBreak) != m_categoryToggle.isOn)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if ((!cardData.CardCategory.Contains(category[0]) != m_categoryToggle.isOn))
+                {
+                    return false;
+                }
+            }
+        }
+
+        if (m_cardCostDropdown.value > 0)
+        {
+            int index = -1;
+            for (var i = 0; i < m_costToggleList.Count; i++)
+            {
+                if (m_costToggleList[i].isOn)
+                {
+                    index = i;
+                    break;
+                }
+            }
+            string optionCostStr = m_cardCostDropdown.options[m_cardCostDropdown.value].text.Replace("\r", "").Replace("\n", "");
+            int optionCost = 0;
+            if (int.TryParse(optionCostStr, out optionCost) && int.TryParse(cardData.Cost.Replace("\r", "").Replace("\n", ""), out int cost))
+            {
+                switch (index)
+                {
+                    case 0: // 同等
+                        if (optionCost != cost)
+                        {
+                            return false;
+                        }
+                        break;
+                    case 1: // 以上
+                        if (optionCost > cost)
+                        {
+                            return false;
+                        }
+                        break;
+                    case 2: // 以下
+                        if (optionCost < cost)
+                        {
+                            return false;
+                        }
+                        break;
+                    default:
+                        return false;
+                }
+            }
+            else if (index == 0)
+            {
+                if (!cardData.Cost.Contains(optionCostStr))
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        if (m_cardRarityDropdown.value > 0)
+        {
+            if (!cardData.CardRarity.Contains(m_cardRarityDropdown.options[m_cardRarityDropdown.value].text))
             {
                 return false;
             }
@@ -464,7 +780,10 @@ public class DeckSceneManager : MonoBehaviour
 
             if (!cardData.CardName.Contains(m_cardNameInputField.text)
                 && !cardData.CardType.Contains(m_cardNameInputField.text)
+                && !cardData.CardSubType.Contains(m_cardNameInputField.text)
                 && !cardData.CardCategory.Contains(m_cardNameInputField.text)
+                && !cardData.Cost.Contains(m_cardNameInputField.text)
+                && !cardData.CardRarity.Contains(m_cardNameInputField.text)
                 && !cardData.Text.Contains(m_cardNameInputField.text))
             {
                 return false;

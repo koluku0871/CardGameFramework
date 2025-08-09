1544c3e244d056d64937cf72da4e768fb0eb9f21
koluku <nttkolukuaquosphone@gmail.com>
2025-08-10 01:36:47 +0900

    [Update]ルーム画面からの遷移時にゲーム設定を保存するように修正

--- Assets/Scripts/Room/PhotonSceneManager.cs
+++ Assets/Scripts/Room/PhotonSceneManager.cs
@@ -494,6 +494,7 @@ public class PhotonSceneManager : MonoBehaviourPunCallbacks
         //m_photonView.RPC(nameof(MoveBattleScene), RpcTarget.AllBuffered);
 
         PhotonNetwork.CurrentRoom.IsOpen = false;
+        m_battlePlayerManager.SetRoomHash();
         PhotonNetwork.LoadLevel("BattleScene");
     }
 
